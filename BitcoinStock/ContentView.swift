7eb70559a5397b8b3fb4a31b7c6fb955550dcf0e
Emin <38878208+EMINBARI@users.noreply.github.com>
2020-08-12 11:25:50 +0300

    Initial Commit

---
+++ BitcoinStock/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BitcoinStock
+//
+//  Created by Emin Bari on 12.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
